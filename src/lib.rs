b0c5c0032091e001857164e990320bdd80f88f6c
dryabrinv <drabrinvalentin@gmail.com>
2024-05-27 14:31:46 +0300

    update event system

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,27 @@
-use std::{any::Any};
+use std::collections::HashMap;
 
-struct EventWriter {
-    events: Vec<Box<dyn Any>>
+pub struct EventBus<T: Clone> {
+    listeners: HashMap<String, Vec<Box<dyn Fn(T)>>>,
 }
 
-impl EventWriter {
-    fn write<T: Event>(&mut self, new_event: T) {
-        self.events.push(Box::new(new_event));
+impl<T: Clone> EventBus<T> {
+    pub fn new() -> Self {
+        EventBus {
+            listeners: HashMap::new(),
+        }
     }
-}
 
-trait Event: Any { }
+    pub fn subscribe<F: 'static + Fn(T)>(&mut self, event_type: &str, listener: F) {
+        self.listeners.entry(event_type.to_string())
+            .or_insert_with(Vec::new)
+            .push(Box::new(listener));
+    }
 
-trait EventHandler<T: Event> {
-    fn read(&mut self, events_writer: &mut EventWriter) {
-        for event in events_writer.events.iter_mut() {
-            if let Some(event) = event.downcast_mut::<T>() {
-                self.on_event(event);
+    pub fn publish(&self, event_type: &str, data: T) {
+        if let Some(listeners) = self.listeners.get(event_type) {
+            for listener in listeners {
+                listener(data.clone());
             }
         }
-    }
-
-    fn on_event(&mut self, event: &T);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    struct Head {
-        sub: Test,
-        events: EventWriter
-    }
-
-    impl Head {
-        fn run(&mut self) {
-            self.sub.run(&mut self.events)
-        }
-    }
-
-    struct Test {
-        values: i32,
-        sub: SubTest
-    }
-
-    impl Test {
-        fn run(&mut self, events: &mut EventWriter) {
-            self.read(events)
-        }
-    }
-
-    impl EventHandler<TestEvent> for Test { 
-        fn on_event(&mut self, event: &TestEvent) {
-            self.values += event.0
-        }
-    }
-
-    struct SubTest {
-
-    }
-
-    struct TestEvent(i32);
-
-    impl Event for TestEvent { }
-
-    #[test]
-    fn it_works() {
-
-
-        let mut test = Head {
-            events: EventWriter { events: Vec::new() },
-            sub: Test {
-                values: 0,
-                sub: SubTest { }
-            }
-        };
-
-        test.events.write(TestEvent(10));
-        test.run();
-
-        assert_eq!(test.sub.values, 10);
     }
 }
